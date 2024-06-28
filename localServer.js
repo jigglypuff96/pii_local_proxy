8fcf19e24f1c9d75360ae645b3bafad3f00a829b
jigglypuff96 <>
2024-06-28 00:36:47 -0400

    add console log

--- localServer.js
+++ localServer.js
@@ -5,7 +5,7 @@ const axios = require("axios");
 
 const app = express();
 const port = 4000; // Local proxy server port
-const vmServerUrl = "https://34.23.212.219:3443"; // URL of the VM server
+const vmServerUrl = "http://34.23.212.219:3000"; // URL of the VM server
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -14,9 +14,11 @@ app.use(bodyParser.json());
 app.post("/detect", async (req, res) => {
   try {
     console.log("detecting...");
+    // console.log("req.body = ", req.body);
     const response = await axios.post(`${vmServerUrl}/detect`, req.body, {
       headers: { "Content-Type": "application/json" },
     });
+    // console.log("detecting response = ", response);
     res.json(response.data);
   } catch (error) {
     console.error(error);
